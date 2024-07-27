a6fd76d3daf78fd5a650eb14880cbaedc9c27e35
CynthiaHe0 <67443885+CynthiaHe0@users.noreply.github.com>
2024-07-27 11:12:26 -0700

    Set up basic files

---
+++ src/Scenes/Hub.js
@@ -0,0 +1,21 @@
+var clone = 0;
+class Hub extends Phaser.Scene {
+    constructor() {
+        super("hubScene");
+    }
+
+    // Use preload to load art and sound assets before the scene starts running.
+    preload() {
+        
+    }
+
+    create() {
+        
+        
+    }
+
+    update() {
+        
+    }
+
+}
