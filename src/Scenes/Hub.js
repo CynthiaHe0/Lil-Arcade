f62104e00847ef4bfa63e8e55d5bad48cb1b0f41
CynthiaHe0 <67443885+CynthiaHe0@users.noreply.github.com>
2024-07-28 19:03:54 -0700

    The lil guy moves now

--- src/Scenes/Hub.js
+++ src/Scenes/Hub.js
@@ -2,39 +2,41 @@ var clone = 0;
 class Hub extends Phaser.Scene {
     constructor() {
         super("hubScene");
+        this.my = {sprite : {}};
     }
 
     // Use preload to load art and sound assets before the scene starts running.
     preload() {
-        
+
     }
 
     create() {
-        my.sprite.player = this.add.image(0, 0, "player");
+        let my = this.my;
+        my.sprite.player = this.add.image(this.game.config.width/2, this.game.config.height/2, "player");
         //Later make a player script and move the controls code there
         this.playerControls = {};
         this.playerControls.up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-        this.playerControls.up.on('down', ()=>{
-            my.sprite.player.y--;
-        });
         this.playerControls.down = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-        this.playerControls.down.on('down', ()=>{
-            my.sprite.player.y++;
-        });
         this.playerControls.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        this.playerControls.right.on('down', ()=>{
-            my.sprite.player.x++;
-        });
         this.playerControls.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        this.playerControls.left.on('down', ()=>{
-            my.sprite.player.x--;
-        });
         this.playerControls.interact = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
     }
 
     update() {
-        
+        let my = this.my;
+        if (this.playerControls.left.isDown){
+            my.sprite.player.x--;
+        }
+        if (this.playerControls.right.isDown){
+            my.sprite.player.x++;
+        }
+        if (this.playerControls.up.isDown){
+            my.sprite.player.y--;
+        }
+        if (this.playerControls.down.isDown){
+            my.sprite.player.y++;
+        }
     }
 
 }
